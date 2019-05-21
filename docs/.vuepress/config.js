023e6fa1f2fe00304b52c81aad798121b47988ad
she_say <1132256103@qq.com>
2019-05-21 18:59:12 +0800

    add more

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,13 +2,15 @@ module.exports = {
     title: '她说',
     description: '我的个人网站',
     head: [ // 注入到当前页面的 HTML <head> 中的标签
-      ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
+      ['link', { rel: 'icon', href: '/img/meat.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
     ],
     base: '/', // 这是部署到github相关的配置
     markdown: {
       lineNumbers: false // 代码块显示行号
     },
     themeConfig: {
+      logo: '/img/logo.jpg',
+      accentColor: '#ac3e40',
       nav:[ // 导航栏配置
         {text: '学习笔记', link: '/' },
         { text: 'Guide', link: 'https://github.com/tashuobutianhei' },
